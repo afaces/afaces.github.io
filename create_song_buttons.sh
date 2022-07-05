3d22fec1a16a0eb14d224845aa76cb6ade8b0663
Axlfc <axelfernandezcurros@gmail.com>
2022-07-05 15:44:18 +0200

    UPDATED: chantes to create_song_buttons

--- create_song_buttons.sh
+++ create_song_buttons.sh
@@ -65,8 +65,7 @@ for album in $(ls | sort -h); do
     echo "###### ${song_title}" >> "${buttons_file}"
     # Embedd function button in index.js and use button to display song player
     title="$(echo ${song_title} | cut -d '-' -f2-)"
-    echo "<script type=\"text/javascript\"></script>
-<script>
+    echo "<script type=\"text/javascript\">
 function showButton${song_tag}() {
   let ${song_tag} = \"<audio controls><source src=\\\"/${repo_relative_path}/${album}/${song}\\\" type=\\\"audio/mp3\\\"></audio>\";
   document.getElementById(\"${song_tag}\").innerHTML = ${song_tag};
