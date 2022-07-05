1399f9509d3d78b80fbbc80fcd1cfc86bbb85e6e
Axel <axelfernandezcurros@gmail.com>
2022-07-05 04:23:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function showButtons() {
+function showButtonnefasto() {
   let nefasto = "<audio controls><source src=\"assets/music/1.- 'Nefasto'/1 - Nefasto.mp3\" type=\"audio/mp3\"></audio>";
-  let forgottenpast = "<audio controls><source src=\"assets/music/1.- 'Nefasto'/2 - Forgotten past.mp3\" type=\"audio/mp3\"></audio>";
 
-  document.getElementById("btn").innerHTML = nefasto;
-  document.getElementById("btn").innerHTML = forgottenpast;
+  document.getElementById("nefasto").innerHTML = nefasto;
+}
+
+function showButtonforgottenpast() {
+  let forgottenpast = "<audio controls><source src=\"assets/music/1.- 'Nefasto'/2 - Forgotten Past.mp3\" type=\"audio/mp3\"></audio>";
+
+  document.getElementById("forgottenpast").innerHTML = forgottenpast;
 }
