c3e993252d78141493abd13f4923342c8c6cc476
Axlfc <axelfernandezcurros@gmail.com>
2022-07-05 02:36:23 +0200

    more js button testing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function showButton() {
-  let nefasto = "<audio controls><source src="assets/music/1.- 'Nefasto'/1 - Nefasto.mp3" type="audio/mp3"></audio>";
+  let nefasto = "<audio controls><source src=\"assets/music/1.- 'Nefasto'/1 - Nefasto.mp3\" type=\"audio/mp3\"></audio>";
   document.getElementById("btn").innerHTML = nefasto;
 }
