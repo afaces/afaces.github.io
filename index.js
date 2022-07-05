4524d8c20c7142744c8825f2bb55014520517633
Axlfc <axelfernandezcurros@gmail.com>
2022-07-05 02:22:33 +0200

    testing javascript button to play nefasto

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function myFunction() {
-  document.getElementById("btn").innerHTML = "<audio controls>
-<source src="assets/music/1.- 'Nefasto'/1 - Nefasto.mp3" type="audio/mp3">
+  var nefasto = "<audio controls>
+<source src=\"assets/music/1.- 'Nefasto'/1 - Nefasto.mp3\" type=\"audio/mp3\">
 </audio>";
+  document.getElementById("btn").innerHTML = nefasto;
 }
