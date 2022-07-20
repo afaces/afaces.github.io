f11a0cb4fe4f810468596e986198281c74e73ece
Axlfc <axelfernandezcurros@gmail.com>
2022-07-20 20:31:33 +0200

    UPDATED: testing more back to top button

--- player.js
+++ player.js
@@ -3065,13 +3065,36 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-$(window).load(function() {
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top");
+const pageProgressBar = document.querySelector(".progress-bar");
 
-  function convert() {
-    var input = document.body.innerHTML;
-    var output = emojione.unicodeToImage(input);
-    document.body.innerHTML = output;
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
+document.addEventListener("scroll", () => {
+  console.log("Scroll Height: ", scrollContainer().scrollHeight);
+  console.log("Client Height: ", scrollContainer().clientHeight);
+
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
   }
-
-  convert();
 });
+
+backToTopButton.addEventListener("click", goToTop);
