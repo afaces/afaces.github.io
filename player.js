a19e327efdab44a086cdc2f8a279870176ce2101
Axlfc <axelfernandezcurros@gmail.com>
2022-07-22 04:22:59 +0200

    CHANGES TO STYLE

--- player.js
+++ player.js
@@ -2938,7 +2938,7 @@ function loadTrack(track_index) {
   track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
   track_name.textContent = track_list[track_index].name;
   track_artist.textContent = track_list[track_index].artist;
-  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+  now_playing.textContent = (track_index + 1) + "/" + track_list.length;
 
   // Set an interval of 1000 milliseconds for updating the seek slider
   updateTimer = setInterval(seekUpdate, 1000);
