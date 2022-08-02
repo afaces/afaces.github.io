debdc0a334660bea6b7994828758e80eba2da617
Axel <axelfernandezcurros@gmail.com>
2022-08-02 16:12:33 +0200

    UPDATED: more jam testing and adding badplaylist logic to spotiscratch.py

--- jam.md
+++ jam.md
@@ -1,15 +1,39 @@
 ## `Casa Lola` Jam sessions:
 
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-session-na-x%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-1%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-2%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-35%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-23%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-13%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-33%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-%25CF%2580%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-iv%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-iv-34%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-5%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-6%2F&amp;hide_cover=1 frameborder=0></iframe>
+
+
 <iframe width=100% height=60 src=https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Faxlfc%2Fcasa-lola-jam-sessions-na-x-7%2F&amp;hide_cover=1 frameborder=0></iframe>
--- spotiscratch.py
+++ spotiscratch.py
@@ -1,11 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-playlists = ["https://open.spotify.com/playlist/2a4YUDpE5KQRDbPT0E9tJ2", "https://open.spotify.com/playlist/5sj85p0DBgtP5GJr7tpAwH"]
-playlistName = ""
+playlists = ["https://open.spotify.com/playlist/2a4YUDpE5KQRDbPT0E9tJ2",
+             "https://open.spotify.com/playlist/0eLED3QLJRWDSb6pTW0zy4",
+             "https://open.spotify.com/playlist/1XbaN56zIWH5HlBhbcFPGe",
+             "https://open.spotify.com/playlist/6Mcta0SrXUh6RQJxZKBD8Y", # 3
+             "https://open.spotify.com/playlist/3Yoekg61qdZYYe6svontIR",
+             "https://open.spotify.com/playlist/5NBtRIkykvSvSV1y1S1XXH",
+             "https://open.spotify.com/playlist/5qnC6nYRLUoPMSqHH93C5L",
+             "https://open.spotify.com/playlist/5DwCADSp8YDO4IOazBMIvx",
+             "https://open.spotify.com/playlist/4OohxMENTJpMJYUy60LV8Q",
+             "https://open.spotify.com/playlist/4kgY11b72mpkbNmyrvBRrK",
+             "https://open.spotify.com/playlist/0FnWquh2lijOZcHqK3UhhC", # 10
+             "https://open.spotify.com/playlist/0t1SdiLXjJEBQsy0GQQuE7",
+             "https://open.spotify.com/playlist/42VoiulLsyXGHHL4Xi5shf",
+             "https://open.spotify.com/playlist/48DCmv7Jr9h8zHUG3cQ6ot",
+             "https://open.spotify.com/playlist/0ylsLGS4PdmkvBWgOFGb4W",
+             "https://open.spotify.com/playlist/3GEWx7EwDDiOMZ9wf8eEFB",
+             "https://open.spotify.com/playlist/0Nw0bzsuihDd1tvAswqOQo",
+             "https://open.spotify.com/playlist/0D2jhjjLba0AFmxrLrn7lw",
+             "https://open.spotify.com/playlist/1yVBgQsd3D7HphwvqNST5r",
+             "https://open.spotify.com/playlist/5UJgK6GlMH9lc3qKssGEnC",
+             "https://open.spotify.com/playlist/0dvjiEDQneQrUiTGjpHb7W", # 20 bad playlist
+             "https://open.spotify.com/playlist/0rgBrrXSUNVfwheUD9IPUd",
+             "https://open.spotify.com/playlist/04t1plohJT1caZZ2z5LBuP",
+             "https://open.spotify.com/playlist/2R7slbNNZWKsmXjivbScwn",
+
+             "https://open.spotify.com/playlist/5sj85p0DBgtP5GJr7tpAwH" # Uh-uh bad playlist
+             ] # Uh- uh and 20 bad playlists
+badplaylists = ["https://open.spotify.com/playlist/5sj85p0DBgtP5GJr7tpAwH", "https://open.spotify.com/playlist/0dvjiEDQneQrUiTGjpHb7W"] # Uh-uh & 20
 artistURL = "/user/1153971537"
 
-url = playlists[0]
+url = playlists[24]
 res = requests.get(url)
 html_page = res.content
 
@@ -23,18 +49,18 @@ blacklist = [
     'script',
     # there may be more elements you don't want, such as "style", etc.
 ]
-
 for t in text:
     if t.parent.name not in blacklist:
         output += '{} '.format(t)
 
 lines = output.split('\n')
-
 namePlaylist = lines[0].split(" ")
+
 result = ""
 for i in namePlaylist[:-3]:
     result = result + i + " "
 namePlaylist = result
+
 print(namePlaylist)
 
 playlist = lines[len(lines) - 1]
@@ -42,6 +68,7 @@ comment = playlist.split("more-icon-android")[0]
 for i in comment.split(" ")[:-4]:
     result = result + i + " "
 commentary = result
+
 #print(commentary)
 
 result = ""
@@ -58,12 +85,26 @@ el = soup.find_all(href=True)
 linesURL = str(el)
 
 myList = []
-index = 1
-for i in playlistList[:-1]:
-    trackURL = linesURL.split(artistURL)[1].split("href")[index].split('"')[1]
-    content = i + ", " + trackURL
-    myList.append(content)
-    index += 2
+
+if url in badplaylists:
+    print("aki printeo si la url está en la badplaylist")
+    print("veo que las dos primeras crashean")
+    index = 0
+    for i in playlistList[:-1]:
+        trackURL = linesURL.split(artistURL)[1].split("href")[index].split('"')[1]
+        content = i + ", " + trackURL
+        myList.append(content)
+        index += 2
+else:
+    index = 1
+    for i in playlistList[:-1]:
+        trackURL = linesURL.split(artistURL)[1].split("href")[index].split('"')[1]
+        content = i + ", " + trackURL
+        myList.append(content)
+        index += 2
+# TODO: Some playlist url don't show link properly due to (what is thought) index desyncronization, musht check sources of difference
+
+
 
 for song in range(len(myList)):
     name = myList[song].split(",")[0]
