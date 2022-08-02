cbe8778318f9d24d3ce1e818b06d9e7f6c8c66f2
Axel <axelfernandezcurros@gmail.com>
2022-08-02 13:33:33 +0200

    UPDATED: spotiscratch.py successfully scrapes 'Temazos de Afaces' playlist (but not other playlists)

--- spotiscratch.py
+++ spotiscratch.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 playlists = ["https://open.spotify.com/playlist/2a4YUDpE5KQRDbPT0E9tJ2", "https://open.spotify.com/playlist/5sj85p0DBgtP5GJr7tpAwH"]
 playlistName = ""
+artistURL = "/user/1153971537"
 
 url = playlists[0]
 res = requests.get(url)
@@ -10,7 +11,6 @@ html_page = res.content
 
 soup = BeautifulSoup(html_page, 'html.parser')
 text = soup.find_all(text=True)
-
 output = ''
 blacklist = [
     '[document]',
@@ -35,7 +35,7 @@ result = ""
 for i in namePlaylist[:-3]:
     result = result + i + " "
 namePlaylist = result
-print(namePlaylist)
+# print(namePlaylist)
 
 playlist = lines[len(lines) - 1]
 comment = playlist.split("more-icon-android")[0]
@@ -54,6 +54,19 @@ firstPosition = result
 playlistList = playlist.split("more-icon-android")
 playlistList[0] = firstPosition
 
-for i in playlistList[:-1]:
-    print(i)
+el = soup.find_all(href=True)
+linesURL = str(el)
 
+myList = []
+index = 1
+for i in playlistList[:-1]:
+    trackURL = linesURL.split(artistURL)[1].split("href")[index].split('"')[1]
+    content = i + ", " + trackURL
+    myList.append(content)
+    index += 2
+
+for song in range(len(myList)):
+    name = myList[song].split(",")[0]
+    songURL = myList[song].split(",")[1]
+    print(name)
+    print(songURL)
