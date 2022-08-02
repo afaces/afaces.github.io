7d59828cfcdd7a209b6e8ac564232e56194a9121
Axel <axelfernandezcurros@gmail.com>
2022-08-02 17:47:55 +0200

    UPDATED: more jam testing ang changes to scrapemixcloud.py

--- scrapemixcloud.py
+++ scrapemixcloud.py
@@ -1,11 +1,9 @@
-
 import subprocess
 
-subprocess.run(["bash", "mixCloudSongData.sh"], stderr=subprocess.PIPE, text=True)
+subprocess.run(["bash", "mixCloudSongData.sh"], stderr=subprocess.PIPE, text=False)
 htmlLines = []
 with open("mixCloudMetadata.txt", "r") as r:
     for line in r:
-        print("hola mama ", line)
         if "\"html\":" in line:
             htmlLines.append(line)
 bashList = str(subprocess.check_output(["head", "-1", "mixCloudSongData.sh"])).replace('"', '')
@@ -16,8 +14,8 @@ iframeList = []
 for line in htmlLines:
     iframeList.append(line.split('"html":')[1].split(',"author_name"')[0].replace('\\', '').replace('height="120"', 'height="60"').replace('"', '')[1:-1])
 
-#print(len(iframeList))
-
+num = len(iframeList) - len(bashList)
 with open("mixCloudTracks.md", "w") as o:
     for k in range(len(bashList)):
-        o.writelines(iframeList[k]+ "\n")
+        print(iframeList[k + num] + "\n\n")
+        o.writelines(iframeList[k + num] + "\n")
